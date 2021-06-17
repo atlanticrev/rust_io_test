9b3dce8d0aa16e1675941278414ea41ab9744a59
atlanticrev <atlanticrev@gmail.com>
2021-06-17 15:11:16 +0300

    unpdate

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -1,31 +1,47 @@
+use std::io::prelude::*;
 use std::fs::{self, File};
 
 // @todo Drop for FileReader
 
 pub struct FileReader {
     fd: Option<File>,
-    byte_array: Option<Vec<u8>>,
+    pub byte_array: Option<Vec<u8>>,
 }
 
 impl FileReader {
-    fn new() -> Self {
+    pub fn new() -> Self {
         FileReader {
             fd: None,
             byte_array: None,
         }
     }
 
-    fn open(&mut self, path: String) {
+    pub fn open(&mut self, path: &str) -> &mut Self {
         match File::open(path) {
             Ok(file) => self.fd = Some(file),
-            Err(err) => println!("{:?}", err)
+            Err(err) => println!("Error open file: {:?}", err),
+        }
+        self
+    }
+
+    pub fn read_to_byte_array(&mut self, path: &str) {
+        match fs::read(path) {
+            Ok(byte_array) => self.byte_array = Some(byte_array),
+            Err(err) => println!("Error read to byte array: {:?}", err),
         }
     }
 
-    fn read_to_byte_array(&mut self, path: String) {
-        match fs::read(path) {
-            Ok(byte_array) => self.byte_array = Some(byte_array),
-            Err(err) => println!("{:?}", err)
+    pub fn read_to_string(&mut self) -> String {
+        let mut string = String::new();
+        match &mut self.fd {
+            Some(file) => {
+                match file.read_to_string(&mut string) {
+                    Ok(num_bytes) => println!("Bytes was read: {:?}", num_bytes),
+                    Err(err) => println!("{:?}", err),
+                }
+            },
+            None => println!("Need to create a FileReader instance first"),
         }
+        string
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod file_reader;
+use file_reader::*;
 
 fn main() {
-    println!("Hello, world!");
+    let mut file_reader = FileReader::new();
+    let string = file_reader
+        .open("./src/test2.txt")
+        .read_to_string();
+    println!("{}", string);
+    
+    file_reader.read_to_byte_array("./test.txt");
+    println!("{:?}", file_reader.byte_array);
 }
