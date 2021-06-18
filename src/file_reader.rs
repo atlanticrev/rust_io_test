2e94fe4b06879cbb9f2db3b3fb9aca13988ddca0
atlanticrev <atlanticrev@gmail.com>
2021-06-18 12:01:27 +0300

    update

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -1,47 +1,21 @@
-use std::io::prelude::*;
 use std::fs::{self, File};
+use std::io::prelude::*;
+use std::io::BufReader;
 
-// @todo Drop for FileReader
-
-pub struct FileReader {
-    fd: Option<File>,
-    pub byte_array: Option<Vec<u8>>,
-}
+pub struct FileReader {}
 
 impl FileReader {
-    pub fn new() -> Self {
-        FileReader {
-            fd: None,
-            byte_array: None,
-        }
+    pub fn read_to_byte_array(path: &str) -> Vec<u8> {
+        let byte_array = fs::read(path).expect("Error read to byte array");
+        byte_array
     }
 
-    pub fn open(&mut self, path: &str) -> &mut Self {
-        match File::open(path) {
-            Ok(file) => self.fd = Some(file),
-            Err(err) => println!("Error open file: {:?}", err),
-        }
-        self
-    }
-
-    pub fn read_to_byte_array(&mut self, path: &str) {
-        match fs::read(path) {
-            Ok(byte_array) => self.byte_array = Some(byte_array),
-            Err(err) => println!("Error read to byte array: {:?}", err),
-        }
-    }
-
-    pub fn read_to_string(&mut self) -> String {
-        let mut string = String::new();
-        match &mut self.fd {
-            Some(file) => {
-                match file.read_to_string(&mut string) {
-                    Ok(num_bytes) => println!("Bytes was read: {:?}", num_bytes),
-                    Err(err) => println!("{:?}", err),
-                }
-            },
-            None => println!("Need to create a FileReader instance first"),
-        }
+    pub fn read_to_string<'a>(string: &'a mut String, path: &'a str) -> &'a mut String {
+        let file = File::open(path).expect("Error open file");
+        let mut buf_reader = BufReader::new(file);
+        buf_reader
+            .read_to_string(string)
+            .expect("Error read to string");
         string
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,10 @@ mod file_reader;
 use file_reader::*;
 
 fn main() {
-    let mut file_reader = FileReader::new();
-    let string = file_reader
-        .open("./src/test2.txt")
-        .read_to_string();
-    println!("{}", string);
-    
-    file_reader.read_to_byte_array("./test.txt");
-    println!("{:?}", file_reader.byte_array);
+    let mut string = String::new();
+    let string = FileReader::read_to_string(&mut string, "./test.vert.txt");
+    println!("[Output] Read to string:\n\n{}\n", string);
+
+    let buf = FileReader::read_to_byte_array("./test.vert.txt");
+    println!("[Output] Read to buffer:\n\n{:?}\n", buf);
 }
